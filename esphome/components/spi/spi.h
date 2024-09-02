28a6437480b729b1aff10fc8786f5c2b4cad7198
Andreas Berg <andreas@edupihi.eu>
2024-09-02 09:53:59 +0300

    spi dev for esp-idf

--- esphome/components/spi/spi.h
+++ esphome/components/spi/spi.h
@@ -207,10 +207,26 @@ class SPIDelegate {
     esph_log_e("spi_device", "variable length write not implemented");
   }
 
-  virtual void write_cmd_addr_data(size_t cmd_bits, uint32_t cmd, size_t addr_bits, uint32_t address,
-                                   const uint8_t *data, size_t length, uint8_t bus_width) {
+  virtual void write_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr,
+                                   const uint8_t *data, size_t length, uint8_t bus_width, uint8_t dummy_bits) {
     esph_log_e("spi_device", "write_cmd_addr_data not implemented");
   }
+
+  virtual void write_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr,
+                                   const uint8_t *data, size_t length, uint8_t bus_width) {
+    write_cmd_addr_data(cmd_bits, cmd, addr_bits, addr, data, length, bus_width, 0);
+  }
+
+  virtual void read_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr,
+                                   uint8_t *data, size_t length, uint8_t bus_width) {
+    read_cmd_addr_data(cmd_bits, cmd, addr_bits, addr, data, length, bus_width, 0);
+  }
+
+  virtual void read_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr,
+                                   uint8_t *data, size_t length, uint8_t bus_width, uint8_t dummy_bits) {
+    esph_log_e("spi_device", "read_cmd_addr_data not implemented");
+  }
+
   // write 16 bits
   virtual void write16(uint16_t data) {
     if (this->bit_order_ == BIT_ORDER_MSB_FIRST) {
@@ -432,9 +448,14 @@ class SPIDevice : public SPIClient {
    * @param length Number of data bytes
    * @param bus_width The number of data lines to use for the data phase.
    */
-  void write_cmd_addr_data(size_t cmd_bits, uint32_t cmd, size_t addr_bits, uint32_t address, const uint8_t *data,
-                           size_t length, uint8_t bus_width = 1) {
-    this->delegate_->write_cmd_addr_data(cmd_bits, cmd, addr_bits, address, data, length, bus_width);
+  void write_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr, const uint8_t *data,
+                           size_t length, uint8_t bus_width = 1, uint8_t dummy_bits = 0) {
+    this->delegate_->write_cmd_addr_data(cmd_bits, cmd, addr_bits, addr, data, length, bus_width, dummy_bits);
+  }
+
+  void read_cmd_addr_data(size_t cmd_bits, uint16_t cmd, size_t addr_bits, uint64_t addr, uint8_t *data,
+                          size_t length, uint8_t bus_width = 1, uint8_t dummy_bits = 0) {
+    this->delegate_->read_cmd_addr_data(cmd_bits, cmd, addr_bits, addr, data, length, bus_width, dummy_bits);
   }
 
   void write_byte(uint8_t data) { this->delegate_->write_array(&data, 1); }
